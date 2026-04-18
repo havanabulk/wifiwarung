f96deb694f6e08dbd0b436e3eb00f8f538f33938
havanabulk <malupribadi@gmail.com>
2026-04-18 11:28:32 +0800

    Update route.js

--- app/api/monitoring/route.js
+++ app/api/monitoring/route.js
@@ -1,1 +1,10 @@
 
+// /app/api/monitoring/route.js
+
+export async function GET() {
+  return Response.json({
+    status: "OK",
+    message: "API monitoring aktif 🚀",
+    time: new Date().toISOString(),
+  });
+}
