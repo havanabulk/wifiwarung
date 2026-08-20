c92cd6b2d34cda1784dd0c68ed975ab843014e24
havanabulk <malupribadi@gmail.com>
2026-08-20 11:53:23 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "WARUNG28 HOTSPOT",
+export const metadata = {
+  metadataBase: new URL("https://warung28.my.id"),
+
+  title: "Warung28 Hotspot WiFi",
+
   description:
-    "WARUNG28 HOTSPOT - Internet cepat, stabil dan fleksibel.",
-};
+    "Pembelian voucher WiFi MikroTik online. Paket 1 Jam, 2 Jam, 3 Jam, Harian, Mingguan dan Bulanan.",
+
+  keywords: [
+    "wifi",
+    "voucher wifi",
+    "hotspot",
+    "mikrotik",
+    "warung28",
+    "wifi bali"
+  ]
+}
 
 export default function RootLayout({
   children,
